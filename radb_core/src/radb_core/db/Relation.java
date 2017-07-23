3d2c9abf4cddc73c0a9eac7d00036ab4a8cafea6
thadumi <thadumi@thadumi-Lenovo-Y50-70>
2017-07-23 19:37:45 +0200

    Added projection operation between two relations

--- radb_core/src/radb_core/db/Relation.java
+++ radb_core/src/radb_core/db/Relation.java
@@ -17,12 +17,16 @@
 package radb_core.db;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import static java.util.Objects.*;
 import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import static java.util.stream.Collectors.*;
 import static java.util.stream.Stream.*;
+import javafx.util.Pair;
 
 /**
  * Defines a relation and the operation that can be done with one or more
@@ -30,7 +34,7 @@ import static java.util.stream.Stream.*;
  *
  * @author Dumitrescu Theodor A.
  */
-public class Relation {
+public class Relation implements Cloneable {
 
     private String name;
     private Header header;
@@ -41,13 +45,17 @@ public class Relation {
         this.header = requireNonNull(header, "The header can't be null");
         this.content = requireNonNull(content, "The content can't be null");
     }
+    
+    public Relation(Header header, Content content) {
+        this("unnamed", header, content);
+    }
 
-    public List<List<String>> getRows(String... attributes) {
+    public List<List<String>> toRecords(String... attributes) {
         return content.getColumns(header.getAttributesIndex(attributes))
                 .collect(toList());
     }
 
-    public Set<List<String>> getRowsAsSet(String... attributes) {
+    public Set<List<String>> toRecordsAsSet(String... attributes) {
         return content.getColumns(header.getAttributesIndex(attributes))
                 .collect(toSet());
     }
@@ -73,14 +81,64 @@ public class Relation {
         }
         final List<List<String>> content = new ArrayList<>(this.content.size());
 
-        this.content.forEach(i -> other.content.forEach(j -> 
-                            content.add(
-                                    concat(i.stream(), j.stream()).collect(toList()))
+        this.content.forEach(i -> other.content.forEach(j
+                -> content.add(
+                        concat(i.stream(), j.stream()).collect(toList()))
         ));
-        
+
         return new Relation("", header.sum(other.header), new Content(content));
     }
-    
-    
+
+    public Relation projection(String... attributes) throws RelationalAlgebraException {
+        return projection(Arrays.asList(attributes));
+    }
+
+    public Relation projection(List<String> attributes) throws RelationalAlgebraException {
+        if (attributes.isEmpty()) return null;
+
+        List<Integer> ids = header.getAttributesIndex(attributes);
+
+        if (ids.isEmpty() || ids.size() != attributes.size()) {
+            RelationalAlgebraException.throwError("Invalid attributes for projection");
+        }
+
+        Header h = new Header(ids.stream().map(i -> header.getAttributes().get(i)).collect(toList()));
+
+        Content c = new Content();
+        content.stream()
+                .map(row
+                    -> ids.stream().map(i -> row.get(i)).collect(toList()))
+                .forEach(c::add);
+        
+
+        return new Relation(header, content);
+    }
+
+    //relation.operator().sum(other).sum(other).valuate();
+    /**
+     * Returns a list with numeric index corresponding to field's name
+     *
+     * @param attributes
+     * @return
+     */
+    public List<Integer> getAttributesIndex(String... attributes) {
+        return header.getAttributesIndex(attributes);
+    }
+
+    public int getAttributeIndex(String attribute) {
+        List<Integer> index = header.getAttributesIndex(attribute);
+
+        return index.isEmpty() ? -1 : index.get(0);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Relation(name, header, content);
+    }
+
+    @Override
+    public String toString() {
+        return header.toString() + "\n" + content.toString();
+    }
 
 }
