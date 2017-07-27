38ca7aaa60acf380fb8bfac653dc546acdd17418
thadumi <dumitrescu_theodor_a_@hotmail.it>
2017-07-27 21:19:33 +0200

    Added factory methods

--- radb_core/src/radb_core/db/Content.java
+++ radb_core/src/radb_core/db/Content.java
@@ -16,43 +16,92 @@
  */
 package radb_core.db;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
-import static java.util.stream.Collectors.*;
-import static radb_core.util.Collections.*;
 import java.util.stream.Stream;
+import static java.util.stream.Collectors.*;
+
+import static radb_core.util.Collections.*;
+import static radb_core.util.Checker.*;
 
 public class Content extends HashSet<List<String>> implements Cloneable{
     
     public static final String EMPTY = "$NULL$";
     
-    public Content(String content) {
-        Stream.of(content.split("\n"))
-              .forEach(this::add);
-    }
     
-    public Content(List<List<String>> c) {
-        super(c);
-    }
+    private int nrElem;
     
-    public Content() {
-        this("");
-    }
+    
+    Content(Collection<List<String>> c) {
+        //super(c);
+        super(c.size());
         
-
-    
-    @Override
-    public String toString() {
-        return stream()
-                   .map(row -> row.stream().collect(joining(", ", "", "")))
-                   .collect(joining("\n", "", ""));
+        /*Iterator i = c.iterator();
+        
+        if(i.hasNext())
+            nrElem = ((List<String>) i.next()).size();
+        else nrElem = -1;
+        */
+        nrElem = -1;
+        
+        c.stream().forEach(this::add);
     }
-
+    
+    Content() {
+        nrElem = -1;
+    }
+    
+    public static Content emptyContent() {
+        return new Content();
+    }
+    
+    public static Content of(String content) {
+        requireNonNull(content, "The content can't be null");
+        throwIf(content, String::isEmpty, "The content is as string with at least one row");
+        
+        Content c = emptyContent();
+        
+        Stream.of(content.split("\n"))
+              .forEach(c::add);
+        
+        return c;
+    }
+    
+    public static Content of(Collection<List<String>> content) {
+        requireNonNull(content, "The content can't be null");
+        
+        return new Content(content);
+    }
+    
+    
     public boolean add(String row) {
         return add(Arrays.asList(row.split(", ")));
+    }
+
+    
+    
+    @Override
+    public boolean add(List<String> e) {
+        throwIf(e , 
+                arg -> Objects.isNull(arg) || arg.isEmpty(), 
+                "The argument can't be null or empty");
+        
+        
+        if(nrElem == -1 && add(e)) {
+            nrElem = e.size();
+            return true;
+        }
+        
+        throwIf(e, 
+                arg -> arg.size() != nrElem,
+                "Can't add a record that hasn't " + nrElem + " elements");
+        
+        return super.add(e);
     }
     
     Stream<List<String>> getColumns(int ... indexes) {
@@ -90,6 +139,13 @@ public class Content extends HashSet<List<String>> implements Cloneable{
     @Override
     public Content clone() {
         return new Content(stream().collect(toList()));
+    }
+    
+    @Override
+    public String toString() {
+        return stream()
+                   .map(row -> row.stream().collect(joining(", ", "", "")))
+                   .collect(joining("\n", "", ""));
     }
     
 }
