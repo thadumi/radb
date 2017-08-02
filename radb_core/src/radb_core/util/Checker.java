041319eea2af645a10927a3c35338aa20a4bc3ed
thadumi <dumitrescu_theodor_a_@hotmail.it>
2017-08-02 16:38:54 +0200

    Add: pattern for throw exception in form If.Throw

--- radb_core/src/radb_core/util/Checker.java
+++ radb_core/src/radb_core/util/Checker.java
@@ -1,11 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  This file is part of RADB Project created by Dumitrescu Theodor A. and Calvi Luigino.
+ *
+ *  Foobar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  RADB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with RADB.  If not, see <http://www.gnu.org/licenses/>.
  */
 package radb_core.util;
 
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
@@ -13,21 +25,81 @@ import java.util.function.Predicate;
  * @author thadumi
  */
 public final class Checker {
-    private Checker(){}
-    
+
+    private Checker() {
+    }
+
     public static <T> T requireNonNull(T t, String mess) {
         return Objects.requireNonNull(t, mess);
     }
     
-    public static <T> T throwIf(T t, Predicate<T> checker, String msg) {
-        if(checker.test(t))
+    
+    public static void throwIf(boolean condition, String msg) {
+        if(condition)
             throw new IllegalArgumentException(msg);
         
-        return t;
     }
     
+    
+    
+    public static <T> T throwIf(T t, Predicate<T> checker, String msg) {
+        throwIf(checker.test(t), msg);
+        return t;
+    }
+
     public static <T> T throwIf(T t, Predicate<T> checker) {
         return throwIf(t, checker, "");
     }
-}
     
+
+    
+    public static <T> T throwIf(T t, Supplier<Boolean> checker, String msg) {
+        throwIf(checker.get(), msg);
+        return t;
+    }
+
+    public static <T> T throwIf(T t, Supplier<Boolean> checker) {
+        return throwIf(t, checker, "");
+    }
+    
+    public static void throwIf(Supplier<Boolean> checker, String msg) {
+        throwIf(checker.get(), msg);
+    }
+    
+    public static void thowIf(Supplier<Boolean> checker) {
+        throwIf(checker, "");
+    }
+    
+    public static class Thrower {
+        boolean condition;
+        //String msg;
+        //Supplier<? extends Throwable> throwableMaker;
+        
+        public static Thrower If(boolean condition) {
+            Thrower t = new Thrower();
+            t.condition = condition;
+            
+            return t;
+        }
+        
+        public <T extends Exception> void Throw(Function<String, T> excemptionMaker, String msg) throws T{
+            if(Objects.isNull(excemptionMaker))
+                return;
+            
+            if(condition)
+                throw excemptionMaker.apply(msg);
+        }
+        
+        public void Throw(Supplier<? extends Exception> excemptionMaker) throws Exception{
+            if(Objects.isNull(excemptionMaker)) excemptionMaker = Exception::new;
+        
+            if(condition)
+                throw excemptionMaker.get();
+        }
+       
+    }
+    
+    public static Thrower If(boolean condition) {
+        return Thrower.If(condition);
+    }
+}
